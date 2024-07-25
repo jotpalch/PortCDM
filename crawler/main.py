a3b29bd93382fb02da5751dbc572bc70e4db38ce
Lewis Luo <a223541730309@gmail.com>
2024-07-25 14:00:40 +0800

    refactor: abstract crawler logic into functions

--- crawler/main.py
+++ crawler/main.py
@@ -1,29 +1,26 @@
+import os
+from typing import Tuple
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Create output directory if not exists
-url = 'https://sdci.kh.twport.com.tw/khbweb/UA1007.aspx'
-response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+def fetch_webpage(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-# Check if the request is successful
-if response.status_code == 200:
+    # Check if the request is successful
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
-    # Save the webpage to output directory
-    with open('output/output.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
-
+def extract_ship_data(html, ids, cols) -> Tuple[bool, pd.DataFrame]:
     # Parse the webpage
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # Define the ids and columns
-    ids = [f"ASPx_船舶即時動態_tccell0_{num}" for num in range(0, 14)]
-    cols = ["船編航次", "船名", "最新事件", "進港申請", "移泊申請", "出港申請", "港外船舶進港", "錨泊中", "進港作業中", "裝卸須知", "移泊作業中", "移泊裝卸作業", "出港作業中", "船舶已出港"]
-    
-    # Extract the data
+    # Extract the ship data
     data = []
     for id in ids:
-        # Find the content by id
+        # Find the ship content by html id
         content = soup.find(id=id)
         if content:
             # Clean the content
@@ -44,10 +41,51 @@ if response.status_code == 200:
             
             data.append(cleaned_content)
         else:
-            data.append("NO")
-
-    df = pd.DataFrame([data], columns=cols)
+            return False, pd.DataFrame()
     
-    df.to_csv('output/output.csv', index=False)
-else:
-    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    return True, pd.DataFrame([data], columns=cols)
+
+def save_to_csv(df, output_path):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+def save_to_html(html, output_path):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, 'w', encoding='utf-8') as file:
+        file.write(html)
+
+def main(url, output_csv_path, output_html_path, ship_content_id_prefix, cols):
+    # Fetch the webpage
+    html = fetch_webpage(url)
+    save_to_html(html, output_html_path)
+
+    # Extract the ship data
+    result_df = pd.DataFrame(columns=cols)
+    ship_id = 0
+    while True:
+        ids = [f"{ship_content_id_prefix}{ship_id}_{num}" for num in range(0, 14)]
+
+        # Check if the ship content exists
+        result, df = extract_ship_data(html, ids, cols)
+        if result == False:
+            break
+
+        # Append the ship data to the dataframe
+        result_df = pd.concat([result_df, df], ignore_index=True)
+
+        # Move to the next ship
+        ship_id += 1
+
+    # Save the ship data to csv
+    save_to_csv(result_df, output_csv_path)
+
+if __name__ == '__main__':
+    # Define the variables
+    url = 'https://sdci.kh.twport.com.tw/khbweb/UA1007.aspx'
+    output_html_path = 'output/output.html'
+    output_csv_path = 'output/output.csv'
+    ship_content_id_prefix = 'ASPx_船舶即時動態_tccell'
+    cols = ["船編航次", "船名", "最新事件", "進港申請", "移泊申請", "出港申請", "港外船舶進港", "錨泊中", "進港作業中", "裝卸須知", "移泊作業中", "移泊裝卸作業", "出港作業中", "船舶已出港"]
+
+    # Run the main function
+    main(url, output_csv_path, output_html_path, ship_content_id_prefix, cols)
