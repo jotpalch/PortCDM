9035f7621db6b2fec391d74f1bdbb8fe63a6e216
jotpalch <jotp076315217@gmail.com>
2024-08-27 10:44:22 +0800

    feat: Update SQL Queries and Conditions in crawler/utils/save.py

--- crawler/utils/save.py
+++ crawler/utils/save.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_batch
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def save_to_csv(df: pd.DataFrame, output_path: str) -> None:
     """
@@ -56,10 +56,7 @@ def save_ship_status_to_db(df: pd.DataFrame) -> None:
             ship_name = EXCLUDED.ship_name,
             latest_event = EXCLUDED.latest_event,
             updated_at = CURRENT_TIMESTAMP
-        WHERE (
-            EXCLUDED.ship_name != ship_status.ship_name OR
-            EXCLUDED.latest_event != ship_status.latest_event
-        )
+        WHERE EXCLUDED.latest_event != ship_status.latest_event
     '''
     data = [(row['船編航次'], row['船名'], row['最新事件']) for _, row in df.iterrows()]
     execute_batch_query(query, data)
@@ -67,25 +64,24 @@ def save_ship_status_to_db(df: pd.DataFrame) -> None:
 def save_ship_berth_order_to_db(df: pd.DataFrame) -> None:
     query = '''
         INSERT INTO ship_berth_order (
-            berth_number, berthing_time, status, pilotage_time,
+            berth_number, berthing_time, ship_status, pilotage_time,
             ship_name_chinese, ship_name_english, port_agent
         ) VALUES (%s, %s, %s, %s, %s, %s, %s)
-        ON CONFLICT (berth_number, ship_name_english) DO UPDATE SET
+        ON CONFLICT (berth_number, ship_name_english, ship_status) DO UPDATE SET
             berthing_time = EXCLUDED.berthing_time,
-            status = EXCLUDED.status,
             pilotage_time = EXCLUDED.pilotage_time,
             ship_name_chinese = EXCLUDED.ship_name_chinese,
             port_agent = EXCLUDED.port_agent
-        WHERE (
-            EXCLUDED.berthing_time != ship_berth_order.berthing_time OR
-            EXCLUDED.status != ship_berth_order.status OR
-            EXCLUDED.pilotage_time != ship_berth_order.pilotage_time OR
-            EXCLUDED.ship_name_chinese != ship_berth_order.ship_name_chinese OR
-            EXCLUDED.port_agent != ship_berth_order.port_agent
-        )
     '''
-    data = [(row['船席'], row['靠泊時間'], row['動態'], row['引水時間'],
-             row['中文船名'], row['英文船名'], row['港代理']) for _, row in df.iterrows()]
+
+    data = [(row['船席'], 
+             convert_time(row['靠泊時間']),
+             row['動態'], 
+             convert_time(row['引水時間']),
+             row['中文船名'], 
+             row['英文船名'], 
+             row['港代理']) for _, row in df.iterrows()]
+    
     execute_batch_query(query, data)
 
 def save_ship_pass_time_to_db(df: pd.DataFrame) -> None:
@@ -93,54 +89,95 @@ def save_ship_pass_time_to_db(df: pd.DataFrame) -> None:
         INSERT INTO ship_voyage (ship_voyage_number, pass_10_miles_time, pass_5_miles_time)
         VALUES (%s, %s, %s)
         ON CONFLICT (ship_voyage_number) DO UPDATE
-        SET pass_10_miles_time = CASE
-                WHEN EXCLUDED.pass_10_miles_time IS NOT NULL THEN EXCLUDED.pass_10_miles_time
-                ELSE ship_voyage.pass_10_miles_time
-            END,
-            pass_5_miles_time = CASE
-                WHEN EXCLUDED.pass_5_miles_time IS NOT NULL THEN EXCLUDED.pass_5_miles_time
-                ELSE ship_voyage.pass_5_miles_time
-            END
+        SET pass_10_miles_time = COALESCE(EXCLUDED.pass_10_miles_time, ship_voyage.pass_10_miles_time),
+            pass_5_miles_time = COALESCE(EXCLUDED.pass_5_miles_time, ship_voyage.pass_5_miles_time),
+            updated_at = CURRENT_TIMESTAMP
+        WHERE 
+            (EXCLUDED.pass_10_miles_time IS DISTINCT FROM ship_voyage.pass_10_miles_time)
+            OR (EXCLUDED.pass_5_miles_time IS DISTINCT FROM ship_voyage.pass_5_miles_time)
     '''
+
     data = [(row['船編航次'], 
-             row['10浬'] if row['10浬'] != 'null' else None, 
-             row['5浬'] if row['5浬'] != 'null' else None) 
+             convert_time(row['10浬']), 
+             convert_time(row['5浬'])) 
             for _, row in df.iterrows()]
     execute_batch_query(query, data)
+
+def convert_time(time_str):
+    if time_str in ['待接靠', 'null', '', None]:
+        return None
+    if '/' in time_str:
+        date_part, time_part = time_str.split(' ')
+        year, month, day = map(int, date_part.split('/'))
+        western_year = year + 1911
+        if len(time_part.split(':')) == 2:
+            time_part += ':00'
+        dt = datetime.strptime(f"{western_year:04d}-{month:02d}-{day:02d} {time_part}", "%Y-%m-%d %H:%M:%S")
+        utc_time = dt - timedelta(hours=8)
+        return utc_time.strftime("%Y-%m-%d %H:%M:%S")
+    return time_str
 
 def save_ship_events_to_db(df: pd.DataFrame) -> None:
     query = '''
         INSERT INTO ship_events (
             ship_voyage_number, event_source, event_time, event_name, 
-            navigation_status, pilot_order_number, berth_code, event_content
+            navigation_status, pilot_order_number, berth_number, event_content_time
         ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         ON CONFLICT (ship_voyage_number, event_time, event_name) DO UPDATE SET
             event_source = EXCLUDED.event_source,
             navigation_status = EXCLUDED.navigation_status,
             pilot_order_number = EXCLUDED.pilot_order_number,
-            berth_code = EXCLUDED.berth_code,
-            event_content = EXCLUDED.event_content
-        WHERE (
-            EXCLUDED.event_source != ship_events.event_source OR
-            EXCLUDED.navigation_status != ship_events.navigation_status OR
-            EXCLUDED.pilot_order_number != ship_events.pilot_order_number OR
-            EXCLUDED.berth_code != ship_events.berth_code OR
-            EXCLUDED.event_content != ship_events.event_content
-        )
+            berth_number = EXCLUDED.berth_number,
+            event_content_time = EXCLUDED.event_content_time
+        WHERE (EXCLUDED.event_content_time IS DISTINCT FROM ship_events.event_content_time)
+            OR (EXCLUDED.event_content_time IS NULL AND ship_events.event_content_time IS NOT NULL)
+            OR (EXCLUDED.event_content_time IS NOT NULL AND ship_events.event_content_time IS NULL)
     '''
-    def convert_to_24h_timestamp(time_str):
-        date, time = time_str.split(' ', 1)
-        period, time= time.rsplit(' ', 1)
-        dt = datetime.strptime(f"{date} {time}", "%Y/%m/%d %I:%M:%S")
-        if period == '下午' and dt.hour != 12:
-            dt = dt.replace(hour=dt.hour + 12)
-        elif period == '上午' and dt.hour == 12:
-            dt = dt.replace(hour=0)
-        return dt.strftime("%Y-%m-%d %H:%M:%S")
+    
+    def process_row(row):
+        event_time = convert_to_24h_timestamp(row['發生時間'])
+        event_content_time = convert_to_timestamp(row['事件內容'])
+        return (
+            row['船編航次'],
+            row['事件來源'],
+            (datetime.strptime(event_time, "%Y-%m-%d %H:%M:%S") - timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S"),
+            row['事件名稱'],
+            row['航行狀態'],
+            row['引水單序號'],
+            row['碼頭代碼'],
+            (datetime.strptime(event_content_time, "%Y-%m-%d %H:%M:%S") - timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S") if event_content_time else None
+        )
 
-    data = [(row['船編航次'], row['事件來源'], convert_to_24h_timestamp(row['發生時間']), row['事件名稱'],
-             row['航行狀態'], row['引水單序號'], row['碼頭代碼'], row['事件內容']) for _, row in df.iterrows()]
+    data = [process_row(row) for _, row in df.iterrows()]
     execute_batch_query(query, data)
+
+def convert_to_24h_timestamp(time_str):
+    date, time = time_str.split(' ', 1)
+    period, time = time.rsplit(' ', 1)
+    dt = datetime.strptime(f"{date} {time}", "%Y/%m/%d %I:%M:%S")
+    if period == '下午' and dt.hour != 12:
+        dt = dt.replace(hour=dt.hour + 12)
+    elif period == '上午' and dt.hour == 12:
+        dt = dt.replace(hour=0)
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+def convert_to_timestamp(time_str):
+    if not time_str or time_str == 'null':
+        return None
+    
+    try:
+        if '/' in time_str:
+            date, time = time_str.split()
+            year, month, day = map(int, date.split('/'))
+            western_year = year + 1911
+            if len(time.split(':')) == 2:
+                time += ':00'
+            return f"{western_year:04d}-{month:02d}-{day:02d} {time}"
+        elif len(time_str) == 12:
+            return f"{time_str[:4]}-{time_str[4:6]}-{time_str[6:8]} {time_str[8:10]}:{time_str[10:12]}:00"
+        return None
+    except:
+        return None
 
 def execute_batch_query(query: str, data: list) -> None:
     with get_db_connection() as conn:
